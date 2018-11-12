6f847b4aa2a4131cf4ec518558a0e4e764e6a6cb
Chris Berthold <cberthold@baxtertechnology.com>
2018-11-12 03:35:40 -0500

    add create todo item service

---
+++ TodoApi/Services/CreateTodoItemService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoApi.Commands;
+using TodoApi.Data;
+using TodoApi.Models;
+
+namespace TodoApi.Services
+{
+    public class CreateTodoItemService
+    {
+        private readonly TodoContext context;
+
+        public CreateTodoItemService(TodoContext context)
+        {
+            this.context = context;
+        }
+
+        public Guid Handle(CreateTodoItem command)
+        {
+            var todoItem = new TodoItem
+            {
+                Description = command.Description,
+                IsDone = command.IsDone,
+            };
+
+            context.TodoItems.Add(todoItem);
+            context.SaveChanges();
+
+            return todoItem.Id;
+        }
+    }
+}
