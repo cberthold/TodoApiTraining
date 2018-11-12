aa6546eef218e27a94af959d4cb398bb8095c69e
Chris Berthold <cberthold@baxtertechnology.com>
2018-11-12 03:20:42 -0500

    get all todo items service

---
+++ TodoApi/Services/GetAllTodoItemsService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoApi.Commands;
+using TodoApi.Data;
+using TodoApi.Models;
+
+namespace TodoApi.Services
+{
+    public class GetAllTodoItemsService
+    {
+        private readonly TodoContext context;
+
+        public GetAllTodoItemsService(TodoContext context)
+        {
+            this.context = context;
+        }
+
+        public IEnumerable<TodoItem> Handle(GetAllTodoItems command)
+        {
+            return context.TodoItems.ToArray();
+        }
+    }
+}
