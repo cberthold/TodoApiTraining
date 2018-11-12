c143c2348ec2524202ba68b9c835147c40be9af9
Chris Berthold <cberthold@baxtertechnology.com>
2018-11-12 03:50:43 -0500

    add delete todo item command

---
+++ TodoApi/Commands/DeleteTodoItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApi.Commands
+{
+    public class DeleteTodoItem
+    {
+        public Guid Id { get; set; }
+    }
+}
