ab639ecee495cfc6412d2a4de2652e4068a5ca41
Chris Berthold <cberthold@baxtertechnology.com>
2018-11-12 03:23:34 -0500

    add get todo item service

---
+++ TodoApi/Services/GetTodoItemService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoApi.Commands;
+using TodoApi.Data;
+using TodoApi.Models;
+
+namespace TodoApi.Services
+{
+    public class GetTodoItemService
+    {
+        private readonly TodoContext context;
+
+        public GetTodoItemService(TodoContext context)
+        {
+            this.context = context;
+        }
+
+        public TodoItem Handle(GetTodoItem command)
+        {
+            var id = command.Id;
+
+            var item = (from i in context.TodoItems
+                        where i.Id == id
+                        select i).FirstOrDefault();
+
+            return item;
+        }
+    }
+}
