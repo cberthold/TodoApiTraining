256d6be35e86357706cb14e9af2c372d104182fd
Chris Berthold <cberthold@baxtertechnology.com>
2018-11-12 03:46:09 -0500

    add update todo item command

---
+++ TodoApi/Commands/UpdateTodoItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoApi.Models;
+
+namespace TodoApi.Commands
+{
+    public class UpdateTodoItem
+    {
+        public Guid Id { get; set; }
+        public string Description { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
