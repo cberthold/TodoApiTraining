7b446c260e2aa467f47579738825599029a6e521
Chris Berthold <cberthold@baxtertechnology.com>
2018-11-12 10:20:22 -0500

    add dbcontext back in

--- TodoApi/Controllers/TodoController.cs
+++ TodoApi/Controllers/TodoController.cs
@@ -15,93 +15,77 @@ namespace TodoApi.Controllers
     [ApiController]
     public class TodoController : ControllerBase
     {
+        private readonly TodoContext context;
+
+        public TodoController(TodoContext context)
+        {
+            this.context = context;
+        }
+
         // GET api/values
         [HttpGet]
         public ActionResult<IEnumerable<TodoItem>> Get()
         {
-            using (var context = CreateTodoContext())
-            {
-                var command = new GetAllTodoItems();
-                var service = new GetAllTodoItemsService(context);
-                var items = service.Handle(command);
-                return Ok(items);
-            }
+            var command = new GetAllTodoItems();
+            var service = new GetAllTodoItemsService(context);
+            var items = service.Handle(command);
+            return Ok(items);
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
         public ActionResult<TodoItem> Get(Guid id)
         {
-            using (var context = CreateTodoContext())
+            var command = new GetTodoItem
             {
-                var command = new GetTodoItem
-                {
-                    Id = id,
-                };
+                Id = id,
+            };
 
-                var service = new GetTodoItemService(context);
-                var item = service.Handle(command);
-                return Ok(item);
-            }
+            var service = new GetTodoItemService(context);
+            var item = service.Handle(command);
+            return Ok(item);
         }
 
         // POST api/values
         [HttpPost]
         public Guid Post([FromBody] TodoItem value)
         {
-            using (var context = CreateTodoContext())
+            var command = new CreateTodoItem
             {
-                var command = new CreateTodoItem
-                {
-                    Description = value.Description,
-                    IsDone = value.IsDone,
-                };
+                Description = value.Description,
+                IsDone = value.IsDone,
+            };
 
-                var service = new CreateTodoItemService(context);
-                var id = service.Handle(command);
-                return id;
-            }
+            var service = new CreateTodoItemService(context);
+            var id = service.Handle(command);
+            return id;
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
         public void Put(Guid id, [FromBody] TodoItem value)
         {
-            using (var context = CreateTodoContext())
+            var command = new UpdateTodoItem
             {
-                var command = new UpdateTodoItem
-                {
-                    Id = id,
-                    Description = value.Description,
-                    IsDone = value.IsDone,
-                };
+                Id = id,
+                Description = value.Description,
+                IsDone = value.IsDone,
+            };
 
-                var service = new UpdateTodoItemService(context);
-                service.Handle(command);
-            }
+            var service = new UpdateTodoItemService(context);
+            service.Handle(command);
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
         public void Delete(Guid id)
         {
-            using (var context = CreateTodoContext())
+            var command = new DeleteTodoItem()
             {
-                var command = new DeleteTodoItem()
-                {
-                    Id = id,
-                };
-                var service = new DeleteTodoItemService(context);
-                service.Handle(command);
-            }
-        }
-
-        private TodoContext CreateTodoContext()
-        {
-            var optionsBuilder = new DbContextOptionsBuilder<TodoContext>();
-            optionsBuilder.UseSqlite("Data Source=todos.db");
-
-            return new TodoContext(optionsBuilder.Options);
+                Id = id,
+            };
+            var service = new DeleteTodoItemService(context);
+            service.Handle(command);
         }
     }
 }
