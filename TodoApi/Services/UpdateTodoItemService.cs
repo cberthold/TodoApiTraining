542e9e079cf48a25dd314d37658def73cdde3423
Chris Berthold <cberthold@baxtertechnology.com>
2018-11-12 03:46:20 -0500

    add update todo item service

---
+++ TodoApi/Services/UpdateTodoItemService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoApi.Commands;
+using TodoApi.Data;
+using TodoApi.Models;
+
+namespace TodoApi.Services
+{
+    public class UpdateTodoItemService
+    {
+        private readonly TodoContext context;
+
+        public UpdateTodoItemService(TodoContext context)
+        {
+            this.context = context;
+        }
+
+        public void Handle(UpdateTodoItem command)
+        {
+            var id = command.Id;
+
+            var item = (from i in context.TodoItems
+                        where i.Id == id
+                        select i).First();
+
+            item.Description = command.Description;
+            item.IsDone = command.IsDone;
+
+            context.SaveChanges();
+        }
+    }
+}
