8df452bf18d46a0956d0fe57463c2b4432feda4a
Chris Berthold <cberthold@baxtertechnology.com>
2018-11-12 03:20:58 -0500

    wire up get all todo items service

--- TodoApi/Controllers/TodoController.cs
+++ TodoApi/Controllers/TodoController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TodoApi.Commands;
+using TodoApi.Data;
+using TodoApi.Models;
+using TodoApi.Services;
 
 namespace TodoApi.Controllers
 {
@@ -10,35 +14,50 @@ namespace TodoApi.Controllers
     [ApiController]
     public class TodoController : ControllerBase
     {
+        private readonly TodoContext context;
+
+        public TodoController(TodoContext context)
+        {
+            this.context = context;
+        }
+
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public ActionResult<IEnumerable<TodoItem>> Get()
         {
-            return new string[] { "value1", "value2" };
+            var command = new GetAllTodoItems();
+            var service = new GetAllTodoItemsService(context);
+            var items = service.Handle(command);
+            return Ok(items);
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        public ActionResult<TodoItem> Get(Guid id)
         {
-            return "value";
+            return new TodoItem()
+            {
+                Id = Guid.NewGuid(),
+                Description = "Item #1",
+                IsDone = false,
+            };
         }
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody] string value)
+        public void Post([FromBody] TodoItem value)
         {
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public void Put(Guid id, [FromBody] TodoItem value)
         {
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public void Delete(Guid id)
         {
         }
     }
